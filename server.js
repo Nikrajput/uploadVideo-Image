18c9efc6d1d7c0e06aee8d93e67f79a695e2073d
Nikrajput <nikrajput8383@gmail.com>
2022-01-16 17:47:16 +0530

    completed

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express=require('express')
+const cloudinary=require('./cloudinary')
+const upload=require('./multer')
+const app=express()
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+
+app.use('/',upload.single("video"),async(req,res) => {
+    console.log(req.file.path);
+    const result=await cloudinary.uploader.upload(req.file.path,{
+        "folder":"YoutubeClone",
+        "resource_type":"video",
+        "quality":"30"
+    });
+    res.json({video:result});
+})
+
+app.listen(process.env.PORT || 4000,()=>{
+    console.log('server running at port')
+})
